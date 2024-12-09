4f00f726bdaab4df42cc45442ca575acc1c2b927
TryonHD <korsch.lenny@gmail.com>
2024-12-09 21:01:28 +0100

    Noch nen bisschen mehr Zeug

--- models.py
+++ models.py
@@ -13,3 +13,21 @@ class Users(db.Model):
     
     last_name = db.Column(db.String(100),
                          nullable=False)
+    
+    company = db.Column(db.String(255),
+                       db.ForeignKey('company.id'),
+                       nullable=False)
+
+class Company(db.Model):
+    
+    __tablename__ = 'company'
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    name = db.Column(db.String(100), 
+                     nullable=False)
+    
+    description = db.Column(db.String(5000), 
+                            nullable=False)
