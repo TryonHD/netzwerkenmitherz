2c353b1accc3c0bcf860df443b09107fd3e7a5f8
TryonHD <korsch.lenny@gmail.com>
2025-05-07 21:25:44 +0200

    Added stuff but gotta talk to MB

--- apps/routes/auth.py
+++ apps/routes/auth.py
@@ -1,17 +1,27 @@
 from apps.models import *
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, session
 
 auth_route = Blueprint('auth_route', __name__)
 
-@auth_route.route("/login/")
-def login():
 
-    return render_template("login.html")
+@auth_route.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        
+        if request.form["submit_button"] == "sub":
+            var = 1
+        return render_template("login.html", **renderdata)
+    elif request.method == "GET":
+        renderdata = {"var": var}
+        return render_template("login.html", **renderdata)
 
 @auth_route.route("/register/")
 def register():
 
-    return render_template("register.html")
+    renderdata = {"email": session["email"],
+                  "password": session["password"]}
+    
+    return render_template("register.html", **renderdata)
 
 @auth_route.route("/logout")
 def logout():
