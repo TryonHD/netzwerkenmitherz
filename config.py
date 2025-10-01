bb5d8f6efa71d7ee78102d4286d8ccd9da6f06bb
Lenny Korsch <kenny@kakton-deb.kakton-deb>
2025-10-01 22:53:28 +0200

    undweirded config

--- config.py
+++ config.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-BASE_PATH = "/netzwerkenmitherz-dev/"
-
-=======
 from os import path
 
 BASE_PATH = path.dirname(__file__)
->>>>>>> 2a851cd (Profile page is better and Input works a little)
